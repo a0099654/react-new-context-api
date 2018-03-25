2169d5615e71f4fee3637b8e7f3cae9a33ea6391
Stella <stella.widyasari@gmail.com>
2018-03-26 07:36:22 +0800

    [Stella]Adding context to the app Both Provider and Consumer present in the same file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import './App.css';
-import Parent from './components/Parent';
 
-let state = {
-  name: 'Stella',
-  age: 27,
-  job: 'Software Developer'
-};
+const myContext = React.createContext();
+
+class MyProvider extends React.Component {
+  state = {
+    name: 'Stella',
+    age: 27,
+    job: 'Software Developer'
+  };
+  render() {
+    return (
+      <myContext.Provider value={"My Value"}>
+        {this.props.children}
+      </myContext.Provider>
+    );
+  }
+}
+
+const SecondChild = () => (
+  <myContext.Consumer>
+    {
+      content => <div>from the second child, here is the {content}</div>
+    }
+  </myContext.Consumer>
+);
+
+const FirstChild = () => (
+  <SecondChild />
+);
 
 const App = () => (
-  <div className="App">
-    <Parent data={state}/>
-  </div>
+  <MyProvider>
+    <div className="App">
+      <FirstChild />
+    </div>
+  </MyProvider>
 );
 
 export default App;
