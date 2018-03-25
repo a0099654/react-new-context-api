a87286abf4ee0a100c11a6259e427364f4fe0ca2
Stella <stella.widyasari@gmail.com>
2018-03-25 14:32:35 +0800

    [Stella]Using React.Fragment

---
+++ src/components/Parent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Parent = () => (
+    <React.Fragment>
+        <div>This is the parent</div>
+        <div>We are using React Fragment Feature</div>
+    </React.Fragment>
+);
+
+export default Parent;
